7d5f38a0e10778a8fc5b41d4b6ebeebfcd57dd28
Duong <trandinhduong841992@gmail.com>
2023-06-28 12:52:18 +0700

    quan ly nhan su

---
+++ ATBM_Seminar/Views/NHANSU/ATBM_Seminar/ModelViews/RoomViewModel.cs
@@ -0,0 +1,52 @@
+﻿using ATBM_Seminar.Models;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATBM_Seminar.ViewModels
+{
+
+    public class RoomViewModel : ViewModelBase
+    {
+        public ObservableCollection<Room> show_AllRoom(OracleConnection conn)
+        {
+            Room room = new Room();
+            ObservableCollection<Room> list_Room = new ObservableCollection<Room>();
+            list_Room = room.allRoom(conn);
+
+            return list_Room;
+        }
+
+        public void updateRoom(OracleConnection connection, string TP, string TrP,string MP)
+        {
+            Room room = new Room();
+            try
+            {
+                room.Update(connection, TP, TrP,MP);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void InsertRoom(OracleConnection connection, string TP, string TrP, string MP)
+        {
+            Room room = new Room();
+            try
+            {
+                room.Insert(connection, TP, TrP, MP);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ ATBM_Seminar/Views/QUANLY_TRUCTIEP/ATBM_Seminar/Models/PhanCong.cs
@@ -0,0 +1,55 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATBM_Seminar.Models
+{
+    public class PhanCong
+    {
+        public string TENNV { get; set; }
+        public string TENDA { get; set; }
+        public string TIME { get; set; }
+        public string Number { get; set; }
+
+
+
+        public ObservableCollection<PhanCong> allPC(string MaNV, OracleConnection conn)
+        {
+            ObservableCollection<PhanCong> list_pc = new ObservableCollection<PhanCong>();
+
+            OracleCommand cmd = new OracleCommand("ATBM_20H3T_22.sp_GetPhanCong", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter cursorParam = cmd.Parameters.Add("EMPLOYCURSOR", OracleDbType.RefCursor);
+            cursorParam.Direction = ParameterDirection.Output;
+
+            OracleParameter inputParam = new OracleParameter("MANV", OracleDbType.Char);
+            inputParam.Value = MaNV;
+            cmd.Parameters.Add(inputParam);
+
+
+            using (OracleDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    PhanCong pc = new PhanCong();
+                    pc.TENNV = reader["TENNV"].ToString();
+                    pc.TENDA = reader["TENDA"].ToString();
+                    pc.TIME = reader["THOIGIAN"].ToString();
+                    pc.Number = (list_pc.Count + 1).ToString();
+                    list_pc.Add(pc);
+                }
+            }
+
+
+            return list_pc;
+        }
+    }
+}
