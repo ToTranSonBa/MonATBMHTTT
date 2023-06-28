a9cabefbb8b2d70300f755d092d343ce03686ae3
Duong <trandinhduong841992@gmail.com>
2023-06-28 10:46:37 +0700

    QUANLY_NHANSU

---
+++ QUANLY_NHANSU/NHANSU/ATBM_Seminar/Models/Room.cs
@@ -0,0 +1,122 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATBM_Seminar.Models
+{
+    public class Room
+    {
+        public string MAPB { get; set; }
+        public string TENPB { get; set; }
+        public string TRPHG { get; set; }
+        public string MATRPHG { get; set; }
+
+        public ObservableCollection<Room> allRoom(OracleConnection conn)
+        {
+            ObservableCollection<Room> list_room = new ObservableCollection<Room>();
+            DataTable result = new DataTable();
+            string query = "SELECT * FROM ATBM_20H3T_22.v_ShowAllPhongBan";
+
+            OracleDataAdapter adapter = new OracleDataAdapter(query, conn);
+            adapter.Fill(result);
+
+
+            for (int i = 0; i < result.Rows.Count; i++)
+            {
+                Room room = new Room();
+                Employee employee = new Employee();
+                DataRow row = result.Rows[i];
+                room.MAPB = row["MAPB"].ToString();
+                room.TENPB = row["TENPB"].ToString();
+                room.MATRPHG = row["TRPHG"].ToString();
+                employee = employee.detailEmployee(row["TRPHG"].ToString(), conn);
+                room.TRPHG = employee.TENNV;
+                list_room.Add(room);
+            }
+
+            return list_room;
+            
+        }
+
+        public Room detailRoom(OracleConnection connection, string MaPB)
+        {
+            Room room = new Room();
+
+
+
+            OracleCommand cmd = new OracleCommand("ATBM_20H3T_22.sp_GetRoom", connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            OracleParameter cursorParam = cmd.Parameters.Add("ROOMCURSOR", OracleDbType.RefCursor);
+            cursorParam.Direction = ParameterDirection.Output;
+
+            OracleParameter inputParam = new OracleParameter("MAPB", OracleDbType.Char);
+            inputParam.Value = MaPB;
+            cmd.Parameters.Add(inputParam);
+
+            using (OracleDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    room.MAPB = reader.GetString(0);
+                    room.TENPB = reader.GetString(1);
+                    room.MATRPHG = reader.GetString(2);
+                }
+            }
+            return room;
+        }
+
+        public void Update(OracleConnection connection, string TP, string TrP,string MP)
+        {
+            OracleCommand cmd = new OracleCommand("ATBM_20H3T_22.sp_UPDATEROOM", connection);
+            cmd.CommandType = CommandType.StoredProcedure; ;
+
+            OracleParameter inputParam2 = new OracleParameter("TP", OracleDbType.NVarchar2);
+            inputParam2.Value = TP;
+            cmd.Parameters.Add(inputParam2);
+
+            OracleParameter inputParam3 = new OracleParameter("TrP", OracleDbType.Char);
+            inputParam3.Value = TrP;
+            cmd.Parameters.Add(inputParam3);
+
+            OracleParameter inputParam4 = new OracleParameter("MP", OracleDbType.Char);
+            inputParam4.Value = MP;
+            cmd.Parameters.Add(inputParam4);
+            
+            using (OracleDataReader reader = cmd.ExecuteReader())
+            {
+
+            }
+        }
+
+        public void Insert(OracleConnection connection, string TP, string TrP, string MP)
+        {
+            OracleCommand cmd = new OracleCommand("ATBM_20H3T_22.sp_INSERTROOM", connection);
+            cmd.CommandType = CommandType.StoredProcedure; ;
+
+            OracleParameter inputParam2 = new OracleParameter("TP", OracleDbType.NVarchar2);
+            inputParam2.Value = TP;
+            cmd.Parameters.Add(inputParam2);
+
+            OracleParameter inputParam3 = new OracleParameter("TrP", OracleDbType.Char);
+            inputParam3.Value = TrP;
+            cmd.Parameters.Add(inputParam3);
+
+            OracleParameter inputParam4 = new OracleParameter("MP", OracleDbType.Char);
+            inputParam4.Value = MP;
+            cmd.Parameters.Add(inputParam4);
+
+            using (OracleDataReader reader = cmd.ExecuteReader())
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ QUANLY_NHANSU/NHANSU/ATBM_Seminar/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,86 @@
+﻿using ATBM_Seminar.Models;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ATBM_Seminar.ViewModels
+{
+
+    public class EmployeeViewModel : ViewModelBase
+    {
+        public ObservableCollection<Employee> show_AllEmp(OracleConnection conn)
+        {
+            Employee emp = new Employee();
+            ObservableCollection<Employee> list_Emp = new ObservableCollection<Employee>();
+            list_Emp = emp.allEmp(conn);
+
+            return list_Emp;
+        }
+
+        public ObservableCollection<Employee> show_AllTP(OracleConnection conn)
+        {
+            Employee TP = new Employee();
+            ObservableCollection<Employee> list_TP = new ObservableCollection<Employee>();
+            list_TP = TP.allTruongPhong( conn);
+
+            return list_TP;
+        }
+
+        public ObservableCollection<Employee> show_AllQL(OracleConnection conn)
+        {
+            Employee QL = new Employee();
+            ObservableCollection<Employee> list_QL = new ObservableCollection<Employee>();
+            list_QL = QL.allQLTrucTiep(conn);
+
+            return list_QL;
+        }
+
+        public ObservableCollection<Employee> show_AllVaiTro(OracleConnection conn)
+        {
+            Employee VaiTro = new Employee();
+            ObservableCollection<Employee> list_VaiTro = new ObservableCollection<Employee>();
+            list_VaiTro = VaiTro.allVaiTro(conn);
+
+            return list_VaiTro;
+        }
+
+        public Employee showDetail_Employee(string MANV, OracleConnection conn)
+        {
+            Employee employee = new Employee();
+            employee = employee.detailEmployee(MANV, conn);
+
+            return employee;
+        }
+
+        public bool updateEmployee(OracleConnection connection, string ma, string ht, string p, string ns, string dc, string sdt, string vt, string ql, string ph)
+        {
+            Employee emp = new Employee();
+            try
+            {
+                return emp.Update(connection, ma, ht, p, ns, dc, sdt, vt, ql, ph);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool insertEmployee(OracleConnection connection, string ma, string ht, string p, string ns, string dc, string sdt, string vt, string ql, string ph)
+        {
+            Employee emp = new Employee();
+            try
+            {
+                return emp.Insert(connection, ma, ht, p, ns, dc, sdt, vt, ql, ph);
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
